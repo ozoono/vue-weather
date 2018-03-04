4b2f1283f485f7df9f7778b538ee22fd3ac45099
David <ozoono@gmail.com>
2018-03-04 20:31:33 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import axios from 'axios'
 export const http = axios.create({
 	baseURL: 'http://api.apixu.com/v1/'
 });
-export const apiKey = 'a0ff64905d7c4632bd294338172108';
+export const apiKey = '<YOUR_API_KEY>';
