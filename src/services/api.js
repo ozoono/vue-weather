44b8f44bca677fb21b457779d020bef872fe638d
David <david@ozoono.com>
2019-10-06 00:19:59 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,4 +3,4 @@ import axios from 'axios'
 export const API = axios.create({
 	baseURL: 'https://api.weatherbit.io/v2.0/'
 });
-export const key = '40a2e65406014595a5894a8c17c5edd7';
+export const key = 'YOUR_API_KEY';
